cec881eb0cdd3f01a92b9d7a80529e1a49b62459
Sheldon Lobo <sheldon.lobo@oracle.com>
2021-01-07 22:23:48 -0500

    Updated javaagent instrumentation: * Allow constructor instrumentation * Verify the only limitation is we need a stack size of >= 2 for simple   method entry print instrumentation

--- java-agent/src/main/java/org/lobo/MethodModifier.java
+++ java-agent/src/main/java/org/lobo/MethodModifier.java
@@ -1,5 +1,6 @@
 package org.lobo;
 
+import org.objectweb.asm.Label;
 import org.objectweb.asm.MethodVisitor;
 import org.objectweb.asm.Opcodes;
 
@@ -7,6 +8,7 @@ public class MethodModifier extends MethodVisitor {
     private String className;
     private String methodName;
     private int numParams;
+    private boolean firstInstruction;
 
     public MethodModifier(MethodVisitor methodVisitor, String className, String methodName) {
         super(Opcodes.ASM7, methodVisitor);
@@ -16,22 +18,36 @@ public class MethodModifier extends MethodVisitor {
 
     @Override
     public void visitParameter(String name, int access) {
-        numParams++;
-        super.visitParameter(name, access);
+        // TODO: Class needs to be compiled with -parameters
+//        numParams++;
+//        System.out.println("  Visit parameter: " + name + " {" + access + "}");
+//        super.visitParameter(name, access);
+    }
+
+    @Override
+    public void visitLocalVariable(String name, String descriptor, String signature, Label start, Label end, int index) {
+        // TODO: Called after visitCode()
+//        System.out.println("Visit local var: " + name);
+//        if (!firstInstruction)
+//            numParams++;
+//        super.visitLocalVariable(name, descriptor, signature, start, end, index);
     }
 
     @Override
     public void visitCode() {
-        System.out.println("Visit method: " + className + "." + methodName + " {" + numParams + "}");
+        System.out.println("Visit method: " + className + "." + methodName + "()");
+        firstInstruction = true;
 
         // Add a print to stdout
-        // TODO: Since we're putting 2 locals on the stack, the method needs to have at least 2 local variable slots
-        if (!methodName.startsWith("<")) {
-            mv.visitFieldInsn(Opcodes.GETSTATIC, "java/lang/System", "out", "Ljava/io/PrintStream;");
+        // TODO: Assume the method has a stack size of at least 2 (since we use 2 stack slots (out, "Executing ..."))
+        mv.visitFieldInsn(Opcodes.GETSTATIC, "java/lang/System", "out", "Ljava/io/PrintStream;");
+        if (methodName.startsWith("<") && methodName.endsWith(">"))
+            mv.visitLdcInsn("Executing constructor: " + className + "." + methodName);
+        else
             mv.visitLdcInsn("Executing method: " + className + "." + methodName);
-            mv.visitMethodInsn(Opcodes.INVOKEVIRTUAL, "java/io/PrintStream", "println", "(Ljava/lang/String;)V",
-                    false);
-        }
+        mv.visitMethodInsn(Opcodes.INVOKEVIRTUAL, "java/io/PrintStream", "println", "(Ljava/lang/String;)V",
+                false);
+        System.out.println("  Instrumented method: " + className + "." + methodName + " {" + numParams + "}");
 
         super.visitCode();
     }
--- java-agent/src/test/java/Loop.java
+++ java-agent/src/test/java/Loop.java
@@ -9,10 +9,30 @@ public class Loop {
         System.out.println(count);
     }
 
+    private int add(int x, int y) {
+        return x + y;
+    }
+
+    private int addTwo(int x) {
+        return x + 2;
+    }
+
+    private static int subTwo(int x) {
+        return x - 2;
+    }
+
+    private static void bar() {
+        System.out.println("In bar");
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello Loop");
         Loop loop = new Loop(10, 20);
+        Loop loop2 = new Loop(100, 200);
         for (int i = 0; i < 5; i++)
-            loop.foo(i, i % 2 == 0);
+            loop.foo(loop2.add(i, i*i), i % 2 == 0);
+        System.out.println("Add 2 returned: " + loop.addTwo(100));
+        System.out.println("Static sub 2 returned: " + subTwo(100));
+        bar();
     }
 }
